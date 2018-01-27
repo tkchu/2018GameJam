5348729d1bf5865e77d9dd96f9594a2b182cf4a6
tkchu <tkchushbm@gmail.com>
2018-01-27 22:10:17 +0800

    变色

---
+++ Assets/ViewAreaColorChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewAreaColorChange : MonoBehaviour {
+    public Color colorIn;
+    public Color colorOut;
+    public SpriteRenderer spriteRenderer;
+
+    public void SetPlayerIn(bool isIn) {
+        if (isIn) {
+            spriteRenderer.color = colorIn;
+        }else {
+            spriteRenderer.color = colorOut;
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            SetPlayerIn(true);
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            SetPlayerIn(false);
+        }
+    }
+}
