171ee21e85b6155c45ab00a4cd3c02d9337db959
tkchu <tkchushbm@gmail.com>
2018-01-27 09:36:51 +0800

    普通人状态机

---
+++ Assets/AngryablePerson.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Facing {
+    down,
+    left,
+    up,
+    right,
+}
+
+public class AngryablePerson : MonoBehaviour {
+    public bool isAngry = false;
+    public Facing facing = Facing.down;
+}
