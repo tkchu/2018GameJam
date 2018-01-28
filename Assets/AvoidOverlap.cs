9af481928fbd266af4cc7c78693e9a96a0d195ff
tkchu <tkchushbm@gmail.com>
2018-01-28 10:54:13 +0800

    避免重叠

---
+++ Assets/AvoidOverlap.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidOverlap : MonoBehaviour {
+    
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y + 10);
+	}
+}
